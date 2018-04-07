d0f9affac7c262e51cc9e293864a95624303adf7
pilgwon <rlavlfrnjs12@gmail.com>
2018-04-08 03:04:34 +0900

    GA Event, Crashlytics added

---
+++ groov/Common/Util/TrackUtil.swift
@@ -0,0 +1,28 @@
+//
+//  TrackUtil.swift
+//  groov
+//
+//  Created by PilGwonKim on 2018. 4. 8..
+//  Copyright © 2018년 PilGwonKim. All rights reserved.
+//
+
+import UIKit
+import FirebaseAnalytics
+
+class TrackUtil: NSObject {
+    
+    class func sendPlaylistAddedEvent(title: String) {
+        Analytics.logEvent("playlist_added", parameters: [
+            AnalyticsParameterItemName: title as NSObject,
+            AnalyticsParameterContentType: "playlist" as NSObject
+            ])
+    }
+
+    class func sendVideoAddedEvent(title: String) {
+        Analytics.logEvent("video_added", parameters: [
+            AnalyticsParameterItemName: title as NSObject,
+            AnalyticsParameterContentType: "video" as NSObject
+            ])
+    }
+    
+}
