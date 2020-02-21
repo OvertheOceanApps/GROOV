274ed760746ea279a84a9420545ad0a300e1baa0
Kyujin Kim <mildwhale@gmail.com>
2020-02-22 02:24:53 +0900

    :art: 비디오 결과 목록 하단에 로딩 뷰 추가. :art: 검색어 입력 시 Debounce 적용.

---
+++ groov/Common/Util/Debouncer.swift
@@ -0,0 +1,33 @@
+//
+//  Debouncer.swift
+//  groov
+//
+//  Created by Kyujin Kim on 03/08/2019.
+//  Copyright © 2019 Mildwhale. All rights reserved.
+//
+import Foundation
+
+final class Debouncer {
+    private var interval: TimeInterval
+    private var timer: Timer?
+    
+    init(interval: TimeInterval) {
+        self.interval = interval
+    }
+    
+    func call(action: @escaping () -> Void) {
+        resetTimer()
+        DispatchQueue.main.async {
+            self.timer = Timer.scheduledTimer(withTimeInterval: self.interval, repeats: false, block: { (_) in
+                action()
+            })
+        }
+    }
+    
+    private func resetTimer() {
+        if timer != nil {
+            timer?.invalidate()
+            timer = nil
+        }
+    }
+}
