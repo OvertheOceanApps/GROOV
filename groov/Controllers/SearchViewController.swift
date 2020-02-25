517aa9ac0cca1722687e6335c2ab2695e79e89bf
Kyujin Kim <mildwhale@gmail.com>
2020-02-25 13:53:41 +0900

    :bug: 추천 검색어 목록을 받아오지 못했을 때, ActivityIndicator가 사라지지 않는 이슈 수정. :art: Project에 DEBUG 플래그 추가. :art: 추천 검색어를 누르면 키보드가 내려가도록 변경.

--- groov/Controllers/SearchViewController.swift
+++ groov/Controllers/SearchViewController.swift
@@ -50,10 +50,10 @@ final class SearchViewController: BaseViewController {
         super.viewDidLoad()
         
         dataManager.delegate = self
+        addKeyboardNotification()
         
         setNavigationBarBackgroundColor()
         initComponents()
-        addKeyboardNotification()
     }
     
     func initComponents() {
@@ -261,6 +261,7 @@ extension SearchViewController: UITableViewDelegate {
         case .suggest:
             let keyword = dataManager.suggestions[indexPath.row]
             searchBar.text = keyword
+            searchBar.resignFirstResponder()
             activityIndicatorView.startAnimating()
             dataManager.requestVideos(suggestion: keyword)
             
